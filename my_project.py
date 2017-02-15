28f7ef195047de248bcbb9440354822a709210af
GriZZly457 <tserber26@gmail.com>
2017-02-15 16:53:08 +0300

    my_project.py

---
+++ my_project.py
@@ -0,0 +1,129 @@
+input('Введите персонажа: ')
+
+
+class Bomber:
+	def __init__(self, name, rarity, type, hp, area_damage, speed, targets, range):
+		self.name = name
+		self.rarity = rarity
+		self.tupe = tupe
+		self.hp = hp
+		self.area_damage = area_damage
+		self.speed = speed
+		self.targets = targets
+		self.dal = dal
+bomb = Bomber("Бомбер","Обычная","Войска", 283, 247, "Средняя","Земля", 4.5)
+print(bomb.name)
+print(bomb.rarity)
+print(bomb.tupe)
+print(bomb.hp)
+print(bomb.area_damage)
+print(bomb.speed)
+print(bomb.targets)
+print(bomb.dal)
+
+class Arrows:
+	def __init__(self, name, rarity, type, area_damage, range, elixir):
+		self.name = name
+		self.rarity = rarity
+		self.tupe = tupe
+		self.area_damage = area_damage
+		self.dal = dal
+		self.elixir = elixir
+arr = Arrows('Стрелы',"Обычная","Заклинание", 221, 4, 3)
+print(arr.name)
+print(arr.rarity)
+print(arr.tupe)
+print(arr.area_damage)
+print(arr.dal)
+print(arr.elixir)
+
+class FireSpirit:
+	def __init__(self, name, rarity, type, hp, area_damage, speed, targets, range, elixir):
+		self.name = name
+		self.rarity = rarity
+		self.tupe = tupe
+		self.hp = hp
+		self.area_damage = area_damage
+		self.speed = speed
+		self.targets = targets
+		self.dal = dal
+		self.elixir = elixir
+fire = FireSpirit("Огненные духи","Обычная","Войска", 82, 154, "Очень быстрая","Земля и воздух", 2, 2)
+print(fire.name)
+print(fire.rarity)
+print(fire.tupe)
+print(fire.hp)
+print(fire.area_damage)
+print(fire.speed)
+print(fire.targets)
+print(fire.dal)
+print(fire.elixir)
+
+class Skeletons:
+	def __init__(self, name, rarity, type, hp, area_damage, speed, targets, range, elixir):
+		self.name = name
+		self.rarity = rarity
+		self.tupe = tupe
+		self.hp = hp
+		self.area_damage = area_damage
+		self.speed = speed
+		self.targets = targets
+		self.dal = dal
+		self.elixir = elixir
+skel = Skeletons("Скелеты","Обычная","Войска", 51, 51, "Быстрая","Земля", 2, 1)
+print(skel.name)
+print(skel.rarity)
+print(skel.tupe)
+print(skel.hp)
+print(skel.area_damage)
+print(skel.speed)
+print(skel.targets)
+print(skel.dal)
+print(skel.elixir)
+
+class Goblins:
+	def __init__(self, name, rarity, type, hp, area_damage, speed, targets, range, elixir):
+		self.name = name
+		self.rarity = rarity
+		self.tupe = tupe
+		self.hp = hp
+		self.area_damage = area_damage
+		self.speed = speed
+		self.targets = targets
+		self.dal = dal
+		self.elixir = elixir
+gobl = Goblins("Гоблины","Обычная","Войска", 128, 80, "Быстрая","Земля", 0, 2)
+print(gobl.name)
+print(gobl.rarity)
+print(gobl.tupe)
+print(gobl.hp)
+print(gobl.area_damage)
+print(gobl.speed)
+print(gobl.targets)
+print(gobl.dal)
+print(gobl.elixir)
+
+class Zap:
+	def __init__(self, name, rarity, type, area_damage, targets, range, elixir):
+		self.name = name
+		self.rarity = rarity
+		self.tupe = tupe
+		self.hp = hp
+		self.area_damage = area_damage
+		self.speed = speed
+		self.targets = targets
+		self.dal = dal
+		self.elixir = elixir
+zap = Zap("Разряд","Обычная","Заклинание", 51, 51, "Быстрая","Земля", 2, 1)
+print(zap.name)
+print(zap.rarity)
+print(zap.tupe)
+print(zap.hp)
+print(zap.area_damage)
+print(zap.speed)
+print(zap.targets)
+print(zap.dal)
+print(zap.elixir)
+
+
+
